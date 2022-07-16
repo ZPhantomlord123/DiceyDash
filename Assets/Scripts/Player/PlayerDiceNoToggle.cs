5244373a2271cec7db1a414dd5804a286b496cd1
ZPhantomlord123 <51522544+ZPhantomlord123@users.noreply.github.com>
2022-07-16 17:52:35 +0530

    Player Sprite, Animation & Movement
    Created player & configured movement and physics

---
+++ Assets/Scripts/Player/PlayerDiceNoToggle.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDiceNoToggle : MonoBehaviour
+{
+    private static int _diceNumber = 1;
+    public Sprite[] _diceSprites;
+    private SpriteRenderer _diceSpriteRenderer;
+    private bool _switchActive = false;
+
+    private void Awake()
+    {
+        _diceSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _diceSpriteRenderer.sprite = _diceSprites[0];
+    }
+
+    public void IncrementNumber()
+    {
+        StartCoroutine(PlayParticleSystem());
+        if (_diceNumber < 6)
+        {
+            _diceNumber++;
+            _diceSpriteRenderer.sprite = _diceSprites[_diceNumber-1];
+        }
+        else
+        {
+            _diceNumber = 1;
+            _diceSpriteRenderer.sprite = _diceSprites[0];
+        }
+    }
+
+    public void DecrementNumber()
+    {
+        StartCoroutine(PlayParticleSystem());
+        if (_diceNumber >1)
+        {
+            _diceNumber--;
+            _diceSpriteRenderer.sprite = _diceSprites[_diceNumber-1];
+        }
+        else
+        {
+            _diceNumber = 6;
+            _diceSpriteRenderer.sprite = _diceSprites[5];
+        }
+    }
+
+    IEnumerator PlayParticleSystem()
+    {
+        _switchActive = true;
+        yield return new WaitForSeconds(0.5f);
+        _switchActive = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") == 1 && !_switchActive)
+        {
+            IncrementNumber();
+        }
+        else if(Input.GetAxisRaw("Vertical") == -1 && !_switchActive)
+        {
+            DecrementNumber();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _playerMoveSpeed;
+    [SerializeField] private float _playerJumpPower;
+    private Rigidbody2D _playerRb;
+
+    private void Start()
+    {
+        _playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Jump()
+    {
+        _playerRb.velocity = (new Vector2(0,1) *_playerJumpPower);
+    }
+
+    private void Move()
+    {
+        var _x = Input.GetAxisRaw("Horizontal");
+        _playerRb.velocity = new Vector3(_x * _playerMoveSpeed, _playerRb.velocity.y, 0);
+    }
+}
