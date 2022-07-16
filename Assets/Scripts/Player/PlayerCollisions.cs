c940270957d2477eb7a0f0134c2dc272dcac9f3b
ZPhantomlord123 <51522544+ZPhantomlord123@users.noreply.github.com>
2022-07-16 20:28:06 +0530

    Player Health, Heal, Take damage
    implemented player health, take damage method and heal method

--- Assets/Scripts/Player/PlayerCollisions.cs
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -4,17 +4,60 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
+    [SerializeField] private PlayerHealthUI _playerHealthUI;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("FallingDice"))
         {
             if(gameObject.GetComponent<PlayerDiceNoToggle>().GetDiceNo() == collision.gameObject.GetComponent<FallingDiceNoToggle>().GetDiceNo())
             {
+                switch (collision.gameObject.GetComponent<FallingDiceNoToggle>().GetDiceNo())
+                {
+                    case 1:
+                        _playerHealthUI.Heal(10);
+                        break;
+                    case 2:
+                        _playerHealthUI.Heal(15);
+                        break;
+                    case 3:
+                        _playerHealthUI.Heal(20);
+                        break;
+                    case 4:
+                        _playerHealthUI.Heal(25);
+                        break;
+                    case 5:
+                        _playerHealthUI.Heal(30);
+                        break;
+                    case 6:
+                        _playerHealthUI.Heal(40);
+                        break;
+
+                }
                 print("heal");
             }
             else
             {
-                print("take dmg");
+                switch (collision.gameObject.GetComponent<FallingDiceNoToggle>().GetDiceNo())
+                {
+                    case 1:
+                        _playerHealthUI.TakeDamage(10);
+                        break;
+                    case 2:
+                        _playerHealthUI.TakeDamage(15);
+                        break;
+                    case 3:
+                        _playerHealthUI.TakeDamage(20);
+                        break;
+                    case 4:
+                        _playerHealthUI.TakeDamage(25);
+                        break;
+                    case 5:
+                        _playerHealthUI.TakeDamage(30);
+                        break;
+                    case 6:
+                        _playerHealthUI.TakeDamage(40);
+                        break;
+                }
             }
         }
     }
---
+++ Assets/Scripts/PlayerHealthUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerHealthUI : MonoBehaviour
+{
+    [SerializeField] private float _playerHealth = 100f;
+    [SerializeField] private TextMeshProUGUI _playerHealthGUIValue;
+    [SerializeField] private Image _playerHealthGUI;
+
+    private void Start()
+    {
+        _playerHealthGUIValue.text = _playerHealth + "%";
+        _playerHealthGUI.fillAmount = (int)_playerHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        _playerHealth -= amount;
+        _playerHealthGUIValue.text = _playerHealth + "%";
+        _playerHealthGUI.fillAmount = (int)_playerHealth;
+        if (_playerHealth <= 0f)
+        {
+            _playerHealth = 0f;
+            //GameOver
+            Time.timeScale = 0f;
+        }
+        
+    }
+
+    public void Heal(float amount)
+    {
+        _playerHealth += amount;
+        if (_playerHealth >= 100f)
+        {
+            _playerHealth = 100f;
+        }
+        _playerHealthGUIValue.text = _playerHealth + "%";
+        _playerHealthGUI.fillAmount = (int)_playerHealth;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
